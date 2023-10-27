b000d32a5f94809fa0bc128381dce783ba4e5463
Alena Sharai <exultantislupus@gmail.com>
2023-10-27 18:57:11 +0400

    customize StarRating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,41 +10,63 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetRating = function () {
+    return;
+  },
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [temprRating, setTemprRating] = useState(0);
 
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTemprRating(i + 1)}
             onHoverOut={() => setTemprRating(0)}
             full={temprRating ? temprRating >= i + 1 : i + 1 <= rating}
+            size={size}
+            color={color}
           />
         ))}
       </div>
-      <p style={textStyle}>{temprRating || rating || ''}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[temprRating ? temprRating - 1 : rating - 1]
+          : temprRating || rating || ''}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: '48px',
-  height: '48px',
-  display: 'block',
-  cursor: 'pointer',
-};
-
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
   return (
     <span
       onMouseEnter={onHoverIn}
@@ -57,8 +79,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
-          fill='#000'
-          stroke='#000'
+          fill={color}
+          stroke={color}
         >
           <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
         </svg>
@@ -67,7 +89,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke='#000'
+          stroke={color}
         >
           <path
             strokeLinecap='round'
