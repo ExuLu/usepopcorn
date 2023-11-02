89b37bcd4d187eb41359c2d2a1ff7db1fc697299
Alena Sharai <exultantislupus@gmail.com>
2023-11-02 17:40:09 +0400

    change focus function to useRef and useEffect combination

--- src/components/NavBar/Search.js
+++ src/components/NavBar/Search.js
@@ -1,11 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Search({ query, setQuery }) {
-  useEffect(function () {
-    const el = document.querySelector('.search');
-    console.log(el);
-    el.focus();
-  });
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === 'Enter') {
+          inputEl.current.focus();
+          setQuery('');
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+      return () => document.removeEventListener('keydown', callback);
+    },
+    [setQuery]
+  );
+
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // });
 
   return (
     <input
@@ -14,6 +33,7 @@ export default function Search({ query, setQuery }) {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
