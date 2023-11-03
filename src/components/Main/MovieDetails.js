2157d60dbc7162995d8ee8b760aefc1bd03c7955
Alena Sharai <exultantislupus@gmail.com>
2023-11-03 16:04:59 +0400

    add useKey user hook for react to key press

--- src/components/Main/MovieDetails.js
+++ src/components/Main/MovieDetails.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import StarRating from './StarRating.js';
 import Loader from './Loader.js';
 import { KEY } from '../../constants.js';
+import { useKey } from '../useKey.js';
 
 export default function MovieDetails({
   selectedId,
@@ -91,22 +92,7 @@ export default function MovieDetails({
     [title]
   );
 
-  useEffect(
-    function () {
-      function callback(e) {
-        if (e.code === 'Escape') {
-          onCloseMovie();
-        }
-      }
-
-      document.addEventListener('keydown', callback);
-
-      return function () {
-        document.removeEventListener('keydown', callback);
-      };
-    },
-    [onCloseMovie]
-  );
+  useKey('Escape', onCloseMovie);
 
   return (
     <div className='details'>
